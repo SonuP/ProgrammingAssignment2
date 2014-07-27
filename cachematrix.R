8e6a4229c6c49e95ba234fed315d6daa1d4ce311
SonuP <suparna.pawar@gmail.com>
2014-07-27 19:29:01 -0400

    Successful commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##I have written a small function just to check the working 
+## of <<- operator. The functions works but assumes at all times 
+## that a  matrix is passed to it.
+## This function creates a special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  set <- function(q) {
+    p <<- q
+    
+  }
+  get <- function() p
 
 }
 
 
-## Write a short comment describing this function
+## This function returns a inverse matrix of special Matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  message("reverse matrix is")
+  m <- solve(c)
+  
+  m
 }
